e7e10ccc6e5cef64b69ceed7cca7aee401e23052
Ian Costanzo <ian@anon-solutions.ca>
2019-06-27 14:23:23 -0700

    Basic web services w.i.p.
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- issuer_controller/src/app.py
+++ issuer_controller/src/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, request, make_response, request
 from flask_script import Manager, Server
 
 import requests
@@ -35,7 +35,7 @@ def custom_call():
         response.raise_for_status()
         did = response.json()
         print("Registered did", did)
-        time.sleep(3)
+        #time.sleep(3)
 
     # register our schema(s) and credential definition(s)
     schema_request = {"schema_name": schema_name, "schema_version": schema_version, "attributes": schema_attrs}
@@ -43,14 +43,29 @@ def custom_call():
     response.raise_for_status()
     schema_id = response.json()
     print("Registered schema", schema_id)
-    time.sleep(3)
+    #time.sleep(3)
 
     cred_def_request = {"schema_id": schema_id['schema_id']}
     response = requests.post(agent_admin_url+'/credential-definitions', json.dumps(cred_def_request))
     response.raise_for_status()
     credential_definition_id = response.json()
     print("Registered credential definition", credential_definition_id)
-    time.sleep(3)
+    #time.sleep(3)
+
+    # register our schema(s) and credential definition(s)
+    schema_request = {"schema_name": schema_name, "schema_version": schema_version, "attributes": schema_attrs}
+    response = requests.post(agent_admin_url+'/schemas', json.dumps(schema_request))
+    response.raise_for_status()
+    schema_id = response.json()
+    print("Registered schema", schema_id)
+    #time.sleep(3)
+
+    cred_def_request = {"schema_id": schema_id['schema_id']}
+    response = requests.post(agent_admin_url+'/credential-definitions', json.dumps(cred_def_request))
+    response.raise_for_status()
+    credential_definition_id = response.json()
+    print("Registered credential definition", credential_definition_id)
+    #time.sleep(3)
 
     # check if we have a TOB connection
     response = requests.get(agent_admin_url+'/connections')
@@ -77,7 +92,7 @@ def custom_call():
         tob_connection = response.json()
 
         print("Established tob connection", tob_connection)
-        time.sleep(3)
+        #time.sleep(3)
 
     # register ourselves (issuer, schema(s), cred def(s)) with TOB
     tob_connection_id = tob_connection['connection_id']
@@ -221,74 +236,103 @@ manager = Manager(app)
 # Remeber to add the command to your Manager instance
 manager.add_command('runserver', CustomServer())
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+
+TOPIC_CONNECTIONS = "connections"
+TOPIC_CREDENTIALS = "credentials"
+TOPIC_PRESENTATIONS = "presentations"
+TOPIC_GET_ACTIVE_MENU = "get-active-menu"
+TOPIC_PERFORM_MENU_ACTION = "perform-menu-action"
+TOPIC_ISSUER_REGISTRATION = "issuer_registration"
+
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
-def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
+@app.route('/api/submit-credential', methods=['POST'])
+def submit_credential():
     if not request.json:
         abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify({'result': True})
+    cred_input = request.json
+
+    # construct and send the credential
+    # TODO look up the cred_def_id based on the supplied schema and version
+    cred_offer = {
+        "connection_id": tob_connection_id,
+        "credential_definition_id": credential_definition_id['credential_definition_id'],
+        "credential_values": cred_input
+    }
+    response = requests.post(agent_admin_url+'/credential_exchange/send', json.dumps(cred_offer))
+    response.raise_for_status()
+    cred_data = response.json()
+
+    # TODO wait for confirmation from the agent, which will include the wallet id of the saved credential
+
+    return jsonify({})
+
+
+def handle_connections(state, message):
+    # TODO auto-accept?
+    print("handle_connections()", state)
+    return Response(state)
+
+def handle_credentials(state, message):
+    # TODO auto-respond to proof requests
+    print("handle_credentials()", state)
+    return Response(some_data)
+
+def handle_presentations(state, message):
+    # TODO auto-respond to proof requests
+    print("handle_presentations()", state)
+    return Response(some_data)
+
+def handle_get_active_menu(message):
+    # TODO add/update issuer info?
+    print("handle_get_active_menu()", message)
+    return Response("")
+
+def handle_perform_menu_action(message):
+    # TODO add/update issuer info?
+    print("handle_perform_menu_action()", message)
+    return Response("")
+
+def handle_register_issuer(message):
+    # TODO add/update issuer info?
+    print("handle_register_issuer()", message)
+    return Response("")
+
+
+@app.route('/api/agent-cb/<topic>', methods=['POST'])
+def agent_callback(topic):
+    if not request.json:
+        abort(400)
+
+    message = request.json
+
+    # dispatch based on the topic type
+    if topic == TOPIC_CONNECTIONS:
+        return handle_connections(message["state"], message)
+
+    elif topic == TOPIC_CREDENTIALS:
+        return handle_credentials(message["state"], message)
+
+    elif topic == TOPIC_PRESENTATIONS:
+        return handle_presentations(message["state"], message)
+
+    elif topic == TOPIC_GET_ACTIVE_MENU:
+        return handle_get_active_menu(message)
+
+    elif topic == TOPIC_PERFORM_MENU_ACTION:
+        return handle_perform_menu_action(message)
+
+    elif topic == TOPIC_ISSUER_REGISTRATION:
+        return handle_register_issuer(message)
+
+    else:
+        print("Callback: topic=", topic, ", message=", message)
+        abort(Response("Invalid topic: " + topic))
+
 
 if __name__ == '__main__':
     manager.run()
