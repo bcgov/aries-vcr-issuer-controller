2241b66257a7781bfd1b72d01863cbf1cc373fe9
Ian Costanzo <ian@anon-solutions.ca>
2019-07-02 13:00:13 -0700

    Integration of config files w.i.p.
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- issuer_controller/src/issuer.py
+++ issuer_controller/src/issuer.py
@@ -5,6 +5,8 @@ import json
 import os
 import time
 import yaml
+import config
+import uuid
 
 
 # list of cred defs per schema name/version
@@ -16,22 +18,44 @@ def startup_init(ENV):
 
     # read configuration files
     config_root = ENV.get('CONFIG_ROOT', '../config')
-    with open(config_root + "/schemas.yml", 'r') as stream:
-        config_schemas = yaml.safe_load(stream)
-    with open(config_root + "/services.yml", 'r') as stream:
-        config_services = yaml.safe_load(stream)
+    config_schemas = config.load_config(config_root + "/schemas.yml", env=ENV)
+    config_services = config.load_config(config_root + "/services.yml", env=ENV)
 
-    #print("schemas.yml -->", json.dumps(config_schemas))
-    #print("services.yml -->", json.dumps(config_services))
+    print("schemas.yml -->", json.dumps(config_schemas))
+    print("services.yml -->", json.dumps(config_services))
 
     # other sample data
-    sample_credential = {
-        "permit_issued_date": "2018-01-01", 
-        "permit_type": "ABC", 
-        "permit_status": "OK", 
-        "effective_date": "2019-01-01", 
-        "corp_num": "ABC12345"
-    }
+    sample_credentials = [
+        {
+            "schema": "ian-registration.ian-ville",
+            "version": "1.0.0",
+            "attributes": {
+                "corp_num": "ABC12345",
+                "registration_date": "2018-01-01", 
+                "entity_name": "Ima Permit",
+                "entity_name_effective": "2018-01-01", 
+                "entity_status": "OK", 
+                "entity_status_effective": "2019-01-01",
+                "entity_type": "ABC", 
+                "registered_jurisdiction": "BC", 
+                "effective_date": "2019-01-01",
+                "expiry_date": ""
+            }
+        },
+        {
+            "schema": "ian-permit.ian-ville",
+            "version": "1.0.0",
+            "attributes": {
+                "permit_id": str(uuid.uuid4()),
+                "entity_name": "Ima Permit",
+                "corp_num": "ABC12345",
+                "permit_issued_date": "2018-01-01", 
+                "permit_type": "ABC", 
+                "permit_status": "OK", 
+                "effective_date": "2019-01-01"
+            }
+        }
+    ]
 
     agent_admin_url = ENV.get('AGENT_ADMIN_URL')
     if not agent_admin_url:
@@ -73,15 +97,16 @@ def startup_init(ENV):
         response = requests.post(agent_admin_url+'/schemas', json.dumps(schema_request))
         response.raise_for_status()
         schema_id = response.json()
-        print("Registered schema", schema_id)
+        app_config['schemas']['SCHEMA_' + schema_name] = schema
         app_config['schemas']['SCHEMA_' + schema_name + '_' + schema_version] = schema_id['schema_id']
+        print("Registered schema", schema_id)
 
         cred_def_request = {"schema_id": schema_id['schema_id']}
         response = requests.post(agent_admin_url+'/credential-definitions', json.dumps(cred_def_request))
         response.raise_for_status()
         credential_definition_id = response.json()
-        print("Registered credential definition", credential_definition_id)
         app_config['schemas']['CRED_DEF_' + schema_name + '_' + schema_version] = credential_definition_id['credential_definition_id']
+        print("Registered credential definition", credential_definition_id)
 
     # what is the TOB connection name?
     tob_connection_params = config_services['verifiers']['bctob']
@@ -111,120 +136,65 @@ def startup_init(ENV):
         tob_connection = response.json()
 
         print("Established tob connection", tob_connection)
+        time.sleep(5)
 
     app_config['TOB_CONNECTION'] = tob_connection['connection_id']
 
     for issuer_name, issuer_info in config_services['issuers'].items():
         # register ourselves (issuer, schema(s), cred def(s)) with TOB
-        issuer_request = {
-            "connection_id": app_config['TOB_CONNECTION'],
-            "issuer_registration": {
-                "credential_types": [
-                    {
+        issuer_config = {
+                    "name": issuer_name,
+                    "abbreviation": issuer_info['abbreviation'],
+                    "did": app_config['DID'],
+                    "email": issuer_info['email'],
+                    "endpoint": issuer_info['endpoint'],
+                    "label": "tbd",
+                    "logo_path": issuer_info['logo_path'],
+                    "url": issuer_info['url']
+                }
+        credential_types = []
+        for credential_type in issuer_info['credential_types']:
+            schema_name = credential_type['schema']
+            schema_info = app_config['schemas']['SCHEMA_' + schema_name]
+            credential_type_info = {
+                        "name": schema_info['name'],
+                        "schema": schema_name,
+                        "topic": credential_type['topic'],
+                        "version": schema_info['version'],
                         "cardinality_fields": {},
                         "category_labels": {},
                         "claim_descriptions": {},
                         "claim_labels": {},
-                        "credential": {
-                            "effective_date": {
-                                "from": "claim",
-                                "input": "effective_date"
-                            }
-                        },
-                        "credential_def_id": "4cLztgZYocjqTdAZM93t27:3:CL:7:default",
-                        "description": "Permit",
-                        "endpoint": "http://localhost:5001/ian-ville/ian-permit",
-                        "logo_b64": "null",
-                        "mapping": [
-                            {
-                                "fields": {
-                                    "format": {
-                                        "from": "value",
-                                        "input": "datetime"
-                                    },
-                                    "type": {
-                                        "from": "value",
-                                        "input": "permit_issued_date"
-                                    },
-                                    "value": {
-                                        "from": "claim",
-                                        "input": "permit_issued_date"
-                                    }
-                                },
-                                "model": "attribute"
-                            },
-                            {
-                                "fields": {
-                                    "type": {
-                                        "from": "value",
-                                        "input": "permit_type"
-                                    },
-                                    "value": {
-                                        "from": "claim",
-                                        "input": "permit_type"
-                                    }
-                                },
-                                "model": "attribute"
-                            },
-                            {
-                                "fields": {
-                                    "type": {
-                                        "from": "value",
-                                        "input": "permit_status"
-                                    },
-                                    "value": {
-                                        "from": "claim",
-                                        "input": "permit_status"
-                                    }
-                                },
-                                "model": "attribute"
-                            },
-                            {
-                                "fields": {
-                                    "format": {
-                                        "from": "value",
-                                        "input": "datetime"
-                                    },
-                                    "type": {
-                                        "from": "value",
-                                        "input": "effective_date"
-                                    },
-                                    "value": {
-                                        "from": "claim",
-                                        "input": "effective_date"
-                                    }
-                                },
-                                "model": "attribute"
-                            }
-                        ],
-                        "name": "tbd",
-                        "schema": "ian-permit.ian-ville",
-                        "topic": {
-                            "source_id": {
-                                "from": "claim",
-                                "input": "corp_num"
-                            },
-                            "type": {
-                                "from": "value",
-                                "input": "registration"
-                            }
-                        },
-                        "version": "1.0.0",
+                        "credential": credential_type['credential'],
+                        "credential_def_id": app_config['schemas']['CRED_DEF_' + schema_name + '_' + schema_info['version']],
+                        "description": schema_info['description'],
+                        "endpoint": credential_type['issuer_url'],
+                        "logo_b64": issuer_info['logo_path'],
+                        "mapping": credential_type['mapping'],
                         "visible_fields": []
                     }
-                ],
-                "issuer": {
-                    "name": alias,
-                    "abbreviation": alias,
-                    "did": app_config['DID'],
-                    "email": "info@ian-ville.ca",
-                    "endpoint": "http://192.168.65.3:5001",
-                    "label": alias,
-                    "logo_path": "../assets/img/ian-ville-logo.jpg",
-                    "url": "https://www.ian-ville.ca/ian-ville-info-page"
-                }
+
+            # config for each attribute
+            if isinstance(schema_info['attributes'], dict):
+                # each element is a dict
+                for attr, desc in schema_info['attributes'].items():
+                    if 'label_en' in desc:
+                        credential_type_info['claim_labels'][attr] = desc['label_en']
+                    if 'description_en' in desc:
+                        credential_type_info['claim_descriptions'][attr] = desc['description_en']
+            if 'cardinality_fields' in credential_type:
+                credential_type_info['cardinality_fields'] = credential_type['cardinality_fields']
+
+            credential_types.append(credential_type_info)
+
+        issuer_request = {
+            "connection_id": app_config['TOB_CONNECTION'],
+            "issuer_registration": {
+                "credential_types": credential_types,
+                "issuer": issuer_config
             }
         }
+        print("Registering issuer", json.dumps(issuer_request))
 
         response = requests.post(agent_admin_url+'/issuer_registration/send', json.dumps(issuer_request))
         response.raise_for_status()
@@ -232,15 +202,18 @@ def startup_init(ENV):
         print("Registered issuer", issuer_name)
 
     # let's send a credential!
-    cred_offer = {
-        "connection_id": tob_connection_id,
-        "credential_definition_id": credential_definition_id['credential_definition_id'],
-        "credential_values": sample_credential
-    }
-    response = requests.post(agent_admin_url+'/credential_exchange/send', json.dumps(cred_offer))
-    response.raise_for_status()
-    cred_data = response.json()
-    print("Sent offer", cred_data)
+    for sample_credential in sample_credentials:
+        credential_definition_id = app_config['schemas']['CRED_DEF_' + sample_credential['schema'] + '_' + sample_credential['version']]
+        cred_offer = {
+            "connection_id": app_config['TOB_CONNECTION'],
+            "credential_definition_id": credential_definition_id,
+            "credential_values": sample_credential['attributes']
+        }
+        response = requests.post(agent_admin_url+'/credential_exchange/send', json.dumps(cred_offer))
+        response.raise_for_status()
+        cred_data = response.json()
+        print("Sent offer", cred_data['credential_exchange_id'], cred_data['connection_id'], cred_data['state'], cred_data['credential_definition_id'])
+        time.sleep(5)
 
     pass
 
