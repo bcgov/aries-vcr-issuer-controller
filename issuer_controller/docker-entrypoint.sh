feaf905b8f173293d48addb69325de4139959b19
Ian Costanzo <ian@anon-solutions.ca>
2019-10-31 09:40:02 -0700

    Requirements for gunicorn startup
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- issuer_controller/docker-entrypoint.sh
+++ issuer_controller/docker-entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 export APP_NAME=${APP_NAME:-app}
+export APP_MODULE=${APP_MODULE:-${APP_NAME}:${APP_NAME}}
 export HOST_IP=${HOST_IP:-0.0.0.0}
-export HOST_PORT=${WEBHOOK_PORT:-5000}
+export HOST_PORT=${HOST_PORT:-5000}
+export WORKER_CONNECTIONS=${WORKER_CONNECTIONS:-1000}
+export WORKER_CLASS=${WORKER_CLASS:-gevent}
 
 CMD="$@"
 if [ -z "$CMD" ]; then
-  CMD="python ${APP_NAME}.py runserver --host=0.0.0.0 --threaded"
+  CMD="gunicorn -k ${WORKER_CLASS} --worker-connections ${WORKER_CONNECTIONS} --bind ${HOST_IP}:${HOST_PORT} ${APP_MODULE}"
 fi
 
 echo "Starting server ..."
