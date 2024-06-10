5eb1838087068e46e579e31f3ad8a69ba05b648a
Akiff Manji <akiff.manji@quartech.com>
2024-06-10 14:50:53 -0700

    feat: use oob with didexchange 1.0 to connect to aries-vcr agent
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- issuer_controller/src/issuer.py
+++ issuer_controller/src/issuer.py
@@ -324,7 +324,13 @@ class StartupProcessingThread(threading.Thread):
                     "agent_admin_url"
                 ]
                 response = requests.post(
-                    tob_agent_admin_url + "/connections/create-invitation",
+                    tob_agent_admin_url + "/out-of-band/create-invitation"
+                    + "?auto_accept=true&use_existing_connection=true",
+                    json.dumps({
+                        "handshake_protocols": [
+                            "https://didcomm.org/didexchange/1.0"
+                        ]
+                    }),
                     headers=TOB_REQUEST_HEADERS,
                 )
                 response.raise_for_status()
@@ -332,8 +338,9 @@ class StartupProcessingThread(threading.Thread):
 
                 response = requests.post(
                     agent_admin_url
-                    + "/connections/receive-invitation?alias="
-                    + tob_connection_params["alias"],
+                    + "/out-of-band/receive-invitation?alias="
+                    + tob_connection_params["alias"]
+                    + "&auto_accept=true&use_existing_connection=true",
                     json.dumps(invitation["invitation"]),
                     headers=ADMIN_REQUEST_HEADERS,
                 )
